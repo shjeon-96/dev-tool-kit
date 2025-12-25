3482db067846ce036f6593303846f6f65613f03a
shjeon-96 <tmdgns893758@gmail.com>
2025-12-25 09:34:34 +0900

    feat: Add Text Case Converter, JSON to TypeScript, CSS Minifier tools, and introduce shared URL state, recent tools, and favorites functionality.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,72 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import boundaries from "eslint-plugin-boundaries";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Helper to force FSD boundaries
+  {
+    plugins: {
+      boundaries,
+    },
+    settings: {
+      "boundaries/include": ["src/**/*"],
+      "boundaries/elements": [
+        {
+          type: "app",
+          pattern: "src/app/**/*",
+        },
+        {
+          type: "widgets",
+          pattern: "src/widgets/**/*",
+        },
+        {
+          type: "features",
+          pattern: "src/features/**/*",
+        },
+        {
+          type: "entities",
+          pattern: "src/entities/**/*",
+        },
+        {
+          type: "shared",
+          pattern: "src/shared/**/*",
+        },
+      ],
+    },
+    rules: {
+      "boundaries/element-types": [
+        "warn",
+        {
+          default: "disallow",
+          rules: [
+            {
+              from: "app",
+              allow: ["app", "widgets", "features", "entities", "shared"],
+            },
+            {
+              from: "widgets",
+              allow: ["widgets", "features", "entities", "shared"],
+            },
+            {
+              from: "features",
+              allow: ["features", "entities", "shared"],
+            },
+            {
+              from: "entities",
+              allow: ["entities", "shared"],
+            },
+            {
+              from: "shared",
+              allow: ["shared"],
+            },
+          ],
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
